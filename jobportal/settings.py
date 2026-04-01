0fdc39118b24946c686eb606ac86d58fa36c2870
rakeshk6110 <rakesrh6767@gmail.com>
2026-04-01 17:33:18 +0530

    fixed error

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -2,10 +2,7 @@
 import os
 from pathlib import Path
 
-try:
-    import dj_database_url
-except ImportError:
-    dj_database_url = None
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
